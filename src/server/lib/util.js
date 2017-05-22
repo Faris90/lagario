bd9544fecea5eb9481db10b221833b27ce59c6f4
( ͡° ͜ʖ ͡°) Fs unlimited <faris.soliman@yahoo.com>
2017-05-22 08:26:24 -0500

    Update util.js

--- src/server/lib/util.js
+++ src/server/lib/util.js
@@ -85,14 +85,6 @@ exports.findIndex = function(arr, id) {
 };
 
 exports.randomColor = function() {
-    var color = '#' + ('00000' + (Math.random() * (1 << 24) | 0).toString(16)).slice(-6);
-    var c = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(color);
-    var r = (parseInt(c[1], 16) - 32) > 0 ? (parseInt(c[1], 16) - 32) : 0;
-    var g = (parseInt(c[2], 16) - 32) > 0 ? (parseInt(c[2], 16) - 32) : 0;
-    var b = (parseInt(c[3], 16) - 32) > 0 ? (parseInt(c[3], 16) - 32) : 0;
-
-    return {
-        fill: color,
-        border: '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+    var color = prompt("Type hex color", "#00000");
     };
 };
